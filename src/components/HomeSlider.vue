0997a4e2f48c7732618d726dff837279e8325aa5
tymodk <tymo.dekock@telenet.be>
2018-10-03 19:42:43 +0200

    Added gift anim

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -5,7 +5,10 @@
 	<img class="slide2" src="img/exotic.png" alt="Second slider">
 	<img class="slide3" src="img/interests.png" alt="Third slider">
 	<img class="slide4" src="img/charity.png" alt="Fourth slider">
-    <img class="backbox" src="img/openbox.png" alt="An open box">    
+	<img class="backbox" src="img/backbox.png" alt="An open box">  
+    <img class="frontbox" src="img/frontbox.png" alt="An open box">    
+      
+
 	
 
     
