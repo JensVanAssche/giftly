2b43c8f2b61bb78e5560bbfa6d61b6701ed86e33
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-03 18:00:11 +0200

    Added category to store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,17 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    currentBox: null,
+    boxOptions: [],
   },
   mutations: {
-
+    changeBox(state, box) {
+      state.currentBox = box;
+    },
   },
-  actions: {
-
-  }
-})
+  actions: {},
+});
