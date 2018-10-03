bba485a029be9f49c555803ca32e93c93a6d39e2
tymodk <tymo.dekock@telenet.be>
2018-10-03 19:31:41 +0200

    Added detailed info

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -1,6 +1,13 @@
 <template>
   <!-- Overview met boxes  -->
-  <div>
-    <img src="https://www.boxedinn.com/wp-content/uploads/boxed-inn-12x9x6-shipping-box.jpg" alt="">
+  <div class="sliders">
+  	<img class="slide1" src="img/random.png" alt="First slider">
+	<img class="slide2" src="img/exotic.png" alt="Second slider">
+	<img class="slide3" src="img/interests.png" alt="Third slider">
+	<img class="slide4" src="img/charity.png" alt="Fourth slider">
+    <img class="backbox" src="img/openbox.png" alt="An open box">    
+	
+
+    
   </div>
 </template>
