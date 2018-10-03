2d2b5b49f6b604a446df6c55a6acebbf13857ef4
perjor <jordy.pereira@hotmail.com>
2018-10-03 14:52:53 +0200

    Made Category Page

--- src/router.js
+++ src/router.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Categories from './views/Categories.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -11,15 +12,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+      path: '/categories',
+      name: 'Categories',
+      component: Categories,
+    },
+  ],
+});
---
+++ src/views/Categories.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul>
+    <li v-for="(category, i) in categories" :key="i">
+      {{ category.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        {
+          name: 'A'
+        },
+        {
+          name: 'B'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
