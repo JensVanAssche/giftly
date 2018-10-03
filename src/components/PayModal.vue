788c34b4e91206b7598b7fdc1816501fc6be86c0
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-03 17:31:03 +0200

    Added pay modal

---
+++ src/components/PayModal.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Address</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputEmail4">Email</label>
+              <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+            </div>
+            <div class="form-group col-md-6">
+              <label for="inputPassword4">Password</label>
+              <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="inputAddress">Address</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+          </div>
+          <div class="form-group">
+            <label for="inputAddress2">Address 2</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-6">
+              <label for="inputCity">City</label>
+              <input type="text" class="form-control" id="inputCity">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputState">State</label>
+              <select id="inputState" class="form-control">
+                <option selected>Choose...</option>
+                <option>...</option>
+              </select>
+            </div>
+            <div class="form-group col-md-2">
+              <label for="inputZip">Zip</label>
+              <input type="text" class="form-control" id="inputZip">
+            </div>
+          </div>
+          <div class="form-group">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck">
+              <label class="form-check-label" for="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Pay</button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -3,14 +3,28 @@
     <h1>Choose price</h1>
     <!-- Image of the box you chose -->
     <!-- <img src="" alt=""> -->
-    <!-- Price and slider to choose price -->
-    <button>Pay</button>
+    <form>
+      <div class="form-group">
+        <label class="display-2">€ {{ price }}</label>
+        <input type="range" class="form-control-range" id="formControlRange" v-model="price">
+      </div>
+    </form>
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Buy</button>
+    <PayModal />
   </div>
 </template>
 
 <script>
+import PayModal from "@/components/PayModal";
 export default {
-
+  components: {
+    PayModal
+  },
+  data() {
+    return {
+      price: 70
+    }
+  }
 }
 </script>
 
