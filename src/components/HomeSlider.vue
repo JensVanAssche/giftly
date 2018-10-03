876a7583bc48dd87792a6dbe7ba0ba11f1e3a2d1
perjor <jordy.pereira@hotmail.com>
2018-10-03 15:48:49 +0200

    Added img to slider

--- src/components/HomeSlider.vue
+++ src/components/HomeSlider.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- Overview met boxes  -->
   <div>
-    
+    <img src="https://www.boxedinn.com/wp-content/uploads/boxed-inn-12x9x6-shipping-box.jpg" alt="">
   </div>
 </template>
