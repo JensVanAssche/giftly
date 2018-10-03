8cea248d53f4b430c8b45df48b06de820b8d8a84
perjor <jordy.pereira@hotmail.com>
2018-10-03 13:13:53 +0200

    Init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Giftly"/>
   </div>
 </template>
 
@@ -16,3 +16,8 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
+
