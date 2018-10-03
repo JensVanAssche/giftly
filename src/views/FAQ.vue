30c04274a7e3ef086e0993458ac1120af4909146
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-03 20:10:00 +0200

    faq list

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -1,13 +1,31 @@
 <template>
   <div class="faq">
-<h1>faq hier pls</h1>
+  <h1>faq hier pls</h1>
+  <ul>
+    <li v-for="(item, i) in faq" :key="i">
+      <span class="d-block lead"> 
+        {{ item.question }}
+      </span>
+      {{ item.answer }}
+    </li>
+  </ul>
   </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      faq: [
+      {
+        question: 'What is?',
+        answer: 'is if.'
+      }
+    ]
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-
 </style>
