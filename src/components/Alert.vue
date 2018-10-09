0d9d5a33eaff918ff2192d9b415ee1882e343b6e
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-09 16:56:52 +0200

    Make alert absolute

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,9 +1,21 @@
 <template>
-  <div v-if="$store.getters.showAlert" class="alert alert-dismissible fade show mt-2" role="alert" :class="[`alert-${$store.state.alert.type}`]">
-    <button type="button" class="close" aria-label="Close" @click="$store.commit('hideAlert')">
-      <span aria-hidden="true">&times;</span>
-    </button>
-    <h4 class="alert-heading">{{ $store.state.alert.header }}</h4>
-    <p>{{ $store.state.alert.message }}</p>
-  </div>
+  <transition name="fade">
+    <div v-if="$store.getters.showAlert" class="alert alert-dismissible fade show mt-2" role="alert" :class="[`alert-${$store.state.alert.type}`]">
+      <button type="button" class="close" aria-label="Close" @click="$store.commit('hideAlert')">
+        <span aria-hidden="true">&times;</span>
+      </button>
+      <h4 class="alert-heading">{{ $store.state.alert.header }}</h4>
+      <p>{{ $store.state.alert.message }}</p>
+    </div>
+  </transition>
 </template>
+
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
