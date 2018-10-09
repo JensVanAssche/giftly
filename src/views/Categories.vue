d66da85a6a7274c51b1635b7a5ced4ff44d8a5c5
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-09 18:59:51 +0200

    Make code responsive again

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -2,7 +2,7 @@
   <div class="categories">
     <h1>Categorieën</h1>
     <div class="row justify-content-around">
-      <div v-for="(category, i) in categories" :key="i" class="col-3">
+      <div v-for="(category, i) in categories" :key="i" class="col-md-4 col-lg-3">
         <Card :key="i" :name="category.name" :info="category.info"> 
           <div class="d-flex justify-content-between">
             <button type="button" class="btn" data-toggle="modal" :data-target="`#checkoutModal${i}`" @click="$store.commit('changeBox', category)">Ik wil deze</button>
@@ -70,7 +70,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-</style>
-
