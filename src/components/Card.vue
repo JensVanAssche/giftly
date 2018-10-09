bea814890e4ff2cca2eb4c9099666b0f02aaeb73
Jordy Pereira <jordy.pereira@hotmail.com>
2018-10-09 18:50:55 +0200

    Shadow on card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div class="card shadow-sm">
     <img class="card-img-top" src="https://davpack.s3.amazonaws.com/h/alt/l/double-wall-boxes_3.jpg" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">{{ name }}</h5>
