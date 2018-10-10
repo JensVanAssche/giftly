bd8ec41d2da15c35f505bfec76d97b7d88fec782
perjor <jordy.pereira@hotmail.com>
2018-10-10 11:32:31 +0200

    Different boxes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="card shadow-sm">
+  <div class="card shadow-sm justify-content-between">
     <img v-if="name == 'Feestdag: Halloween Editie'" class="card-img-top" src="/img/box_halloween.png" alt="Card image cap">
-    <img v-else class="card-img-top py-2" src="../assets/img/presentBox.png" alt="Card image cap">
-    <div class="card-body">
+    <img v-else-if="boxImg" class="card-img-top py-2" :src="`/img/boxes/${boxImg}`" alt="Card image cap">
+    <img v-else class="card-img-top p-1" src="../assets/img/presentBox.png" alt="Card image cap">
+    <div class="card-body d-flex flex-column justify-content-between">
       <h5 class="card-title">{{ name }}</h5>
       <p class="card-text">{{ info }}</p>
       <slot></slot>
@@ -14,14 +15,18 @@
 export default {
   props: {
     name: String,
-    info: String
+    info: String,
+    boxImg: String,
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.card {
+  min-height: 400px;
+}
 img {
-  max-width: 250px;
+  max-width: 150px;
   height: auto;
   margin: 0 auto;
 }
