8558ad80bddb1c877c2911f14d0ec3248b809b08
perjor <jordy.pereira@hotmail.com>
2018-10-10 11:02:51 +0200

    Changed box image

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card shadow-sm">
     <img v-if="name == 'Feestdag: Halloween Editie'" class="card-img-top" src="/img/box_halloween.png" alt="Card image cap">
-    <img v-else class="card-img-top" src="/img/box.jpg" alt="Card image cap">
+    <img v-else class="card-img-top py-2" src="../assets/img/presentBox.png" alt="Card image cap">
     <div class="card-body">
       <h5 class="card-title">{{ name }}</h5>
       <p class="card-text">{{ info }}</p>
@@ -19,5 +19,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+img {
+  max-width: 250px;
+  height: auto;
+  margin: 0 auto;
+}
 </style>
+
