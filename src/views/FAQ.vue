6047f6be62dd59b0010e524c23984362a4b97061
tymo.dekock <tymo.dekock@telenet.be>
2018-10-17 13:23:28 +0200

    Added FAQ vragen

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -18,8 +18,26 @@ export default {
     return {
       faq: [
       {
-        question: 'Waar zijn de vragen?',
-        answer: 'Ze komen eraan na onze user testing!'
+        question: 'Hoe werkt Giftly?',
+        answer: 'Giftly is een nieuwe service die de manier van pakjes kopen gaat veranderen! Je kent wel de momenten, vlak voor kerstmis of een verjaardag, je wil voor iemand een cadeau kopen maar hebt geen idee hebt hoe of wat, waar of wanneer. Die tijden van kopbreken zijn voorbij dankzij Giftly! Bij ons kan je geweldige cadeaus kopen waarvan je ontvanger noch jezelf weet wat erin zit, een dubbele verrassing dus! Cadeaus kunnen gekocht worden op basis van interesses, lokale of exotische afkomst of gewoon totaal willekeurig! Aan het eind van uw aankoop krijgt u de kans om uw prijs aan te passen. Hoe meer u betaalt, hoe meer u in het cadeautje steekt! De inhoud van het cadeau hangt af van uw keuzes bij het bestel-proces.'
+      },
+      {
+        question: 'Weet ik wat er in het cadeau zit eens ik het koop?',
+        answer: 'Neen! U zult weten wat voor soort cadeau het is, behalve als het willekeurig is. Zo is het een verassing voor iedereen!'
+      },
+      
+      {
+        question: 'Ik heb een aankoop gedaan, wat nu?',
+        answer: 'U zal een e-mail krijgen met daarin in een tracking-ID van uw lokale pakjes-dienst. Zo kan u uw pakje opvolgen.'
+      },
+      
+      {
+        question: 'Kan ik mijn pakje personalizeren?',
+        answer: 'Voor dat u betaalt kan u uw pakje aanpassen naar uw persoonlijke voorkeuren. Zo kan u inpakpapier kiezen en zelf een persoonlijk kaartje toevoegen aan het pakje.'
+      },
+      {
+        question: 'Ik vind mijn pakje niet leuk, wat doe ik dan?',
+        answer: 'Wij zijn verplicht om uw pakjes terug te nemen, volgens Europese wet. U zal dan krediet krijgen voor onze winkel.'
       }
     ]
     }
