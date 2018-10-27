987d6970a5bc63a606e827b98e0e455c17ecb105
JensVanAssche <jens.va@hotmail.com>
2018-10-27 21:37:52 +0200

    testimonial cardé

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -65,7 +65,22 @@
           </div>
         </div>
         <div class="col-12">
-          <p>a</p>
+          <h3 class="text-center mb-4">
+            Lees wat anderen te zeggen hebben over Giftly
+          </h3>
+          <div class="row">
+            <div class="col-3"></div>
+            <div class="col-6">
+              <div class="card shadow-sm p-3 mb-4">
+                <div class="d-flex mb-3">
+                  <img src="../assets/img/profo_jens.jpg" class="profo mr-3">
+                  <h2>Jens zegt:</h2>
+                </div>
+                <p class="testimonial_quote">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+              </div>
+            </div>
+            <div class="col-3"></div>
+          </div>
         </div>
       </div>
     </div>
