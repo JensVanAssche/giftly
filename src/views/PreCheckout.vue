c7881c14eaa9f856822fe1454e453aec2bfe7a6f
Jordy White <jordy.pereira@hotmail.com>
2018-11-02 05:16:45 +0100

    v-model needs an existing data prop

--- src/views/PreCheckout.vue
+++ src/views/PreCheckout.vue
@@ -7,9 +7,9 @@
 			<div class="col-4"></div>
 			<div class="card shadow-sm p-3 col-4">
 				<h2 class="pb-3">Inloggen</h2>
-				<input type="text" name="name" placeholder="Naam" v-model="loginName" class="form-control mb-3">
-				<input type="password" name="password" placeholder="Wachtwoord" class="form-control mb-3">
-				<router-link to="/checkout" class="btn btn-red" @click.native="$store.commit('changeLogin', loginName)">Inloggen</router-link>
+				<input type="text" name="name" placeholder="Naam" v-model="username" class="form-control mb-3">
+				<input type="password" name="password" placeholder="Wachtwoord" v-model="password" class="form-control mb-3">
+				<router-link to="/checkout" class="btn btn-red" @click.native="$store.commit('changeLogin', username)">Inloggen</router-link>
 				<hr />
 				<p>of</p>
 				<router-link to="/checkout" class="btn btn-red">Doorgaan zonder inloggen</router-link>
@@ -19,4 +19,13 @@
 	</div>
 </template>
 
-<script></script>
+<script>
+export default {
+	data() {
+		return {
+			username: '',
+			password: '',
+		}
+	}
+}
+</script>
