df2ebdcb596f3f7aff27b162c67bb7174c063f96
Jordy Pereira <jordy.pereira@hotmail.com>
2018-11-10 16:31:52 +0100

    Delete Single Item From Store fixes #28

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { data } from './modules/data';
+import Vue from 'vue'
+import Vuex from 'vuex'
+import { data } from './modules/data'
 
-const uuidv4 = require('uuid/v4');
+const uuidv4 = require('uuid/v4')
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
@@ -33,78 +33,86 @@ export default new Vuex.Store({
       state.currentBox = {
         type,
         ...state.currentBox,
-      };
+      }
     },
     changeLogin(state, login) {
-      state.loginName = login;
+      state.loginName = login
     },
     updatePrice(state, price) {
-      state.price = price;
+      state.price = price
     },
     clearCurrentBox: state => {
-      state.price = 10;
+      state.price = 10
       state.currentBox = {
         age: 'Alle leeftijden',
         sex: 'Beide',
-      };
+      }
     },
     showAlert(state, { header, message, type }) {
-      state.alert.header = header;
-      state.alert.message = message;
-      state.alert.type = type;
+      state.alert.header = header
+      state.alert.message = message
+      state.alert.type = type
     },
     hideAlert(state) {
-      state.alert.header = null;
-      state.alert.message = null;
-      state.alert.type = 'success';
+      state.alert.header = null
+      state.alert.message = null
+      state.alert.type = 'success'
     },
     changeBoxOptions(state, options) {
-      state.currentBoxOptions = options;
+      state.currentBoxOptions = options
     },
     addToCart(state, box) {
-      let uuid = uuidv4();
-      box = { uuid, ...box };
+      let uuid = uuidv4()
+      box = { uuid, ...box }
 
-      state.shoppingCart.push(box);
-      localStorage.setItem('shoppingCart', JSON.stringify(state.shoppingCart));
+      state.shoppingCart.push(box)
+      localStorage.setItem('shoppingCart', JSON.stringify(state.shoppingCart))
     },
     clearCart(state) {
-      state.shoppingCart = [];
-      localStorage.removeItem('shoppingCart');
+      state.shoppingCart = []
+      localStorage.removeItem('shoppingCart')
+    },
+    deleteFromCart(state, uuid) {
+      const updatedCart = state.shoppingCart.filter(item => {
+        return item.uuid !== uuid
+      })
+
+      state.shoppingCart = updatedCart
+      localStorage.setItem('shoppingCart', JSON.stringify(updatedCart))
     },
   },
   actions: {
     hideAlert({ commit }) {
       setTimeout(() => {
-        commit('hideAlert');
-      }, 1000);
+        commit('hideAlert')
+      }, 1000)
     },
   },
   getters: {
     showAlert: state => {
       if (state.alert.header == null && state.alert.message == null) {
-        return false;
+        return false
       }
-      return true;
+      return true
     },
     // Calculates the price of all boxes in the shoppingCart
     totalPrice: state => {
-      let price = 0;
+      let price = 0
 
       state.shoppingCart.map(box => {
-        price += box.price;
-      });
+        price += box.price
+      })
 
-      return price;
+      return price
     },
     totalItemsInCart: state => {
-      return state.shoppingCart.length;
+      return state.shoppingCart.length
     },
     isLoggedIn: state => {
-      return state.loginName ? true : false;
+      return state.loginName ? true : false
     },
     precheckoutOrCheckout: (state, getters) => {
-      return getters.isLoggedIn ? 'checkout' : 'precheckout';
+      return getters.isLoggedIn ? 'checkout' : 'precheckout'
     },
   },
-});
+})
