80f0a4a485482526949e8f0d1842a6b6192b47c5
Jordy Pereira <jordy.pereira@hotmail.com>
2018-11-11 02:48:02 +0100

    Deleted whitespace and ol shopping icon

--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,15 +1,13 @@
 <template>
 	<div class="alertcontainer">
-		
-	
-  <transition name="fade">
-    <div v-if="$store.getters.showAlert" class="alert alert-dismissible fade show mt-2" role="alert" :class="[`alert-${$store.state.alert.type}`]">
-      <button type="button" class="close" aria-label="Close" @click="$store.commit('hideAlert')">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <h4 class="alert-heading">{{ $store.state.alert.header }}</h4>
-      <p>{{ $store.state.alert.message }}</p>
-    </div>
-  </transition>
+    <transition name="fade">
+      <div v-if="$store.getters.showAlert" class="alert alert-dismissible fade show mt-2" role="alert" :class="[`alert-${$store.state.alert.type}`]">
+        <button type="button" class="close" aria-label="Close" @click="$store.commit('hideAlert')">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <h4 class="alert-heading">{{ $store.state.alert.header }}</h4>
+        <p>{{ $store.state.alert.message }}</p>
+      </div>
+    </transition>
   </div>
 </template>
